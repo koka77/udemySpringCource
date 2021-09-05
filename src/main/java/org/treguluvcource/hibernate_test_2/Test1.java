6e362e580c3556148d01048c662191b1588a2fd6
asomov_dev <debalast@ya.ru>
2021-09-05 18:49:09 +0300

    получение данных из бд каскадом one_by_one part_2

--- src/main/java/org/treguluvcource/hibernate_test_2/Test1.java
+++ src/main/java/org/treguluvcource/hibernate_test_2/Test1.java
@@ -8,24 +8,46 @@ import org.treguluvcource.hibernate_test_2.entity.Employee;
 
 public class Test1 {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration().
+
+
+        try (SessionFactory factory = new Configuration().
                 configure("hibernate.cfg.xml").
                 addAnnotatedClass(Employee.class).
                 addAnnotatedClass(Detail.class).
                 buildSessionFactory();
+             Session session = factory.getCurrentSession()) {
+//            Employee employee = new Employee("Alex", "Somov", "IT", 500);
+//            Detail detail = new Detail("Moscow", "+71234567", "mail@mail.com");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
 
-        try (Session session = factory.getCurrentSession()){
-            Employee employee = new Employee("Alex", "Somov", "IT", 500);
-            Detail detail = new Detail("Moscow", "+71234567", "mail@mail.com");
-
-            employee.setEmpDetail(detail);
+//            Employee employee = new Employee("Oleg", "Smirnov", "Saleas", 700);
+//            Detail detail = new Detail("Sarov", "+7123-123", "mail.oleg@mail.com");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
 
             session.beginTransaction();
 
-            session.save(employee);
+            Employee emp = session.get(Employee.class, 10);
+            System.out.println(emp.getEmpDetail());
 
             session.getTransaction().commit();
             System.out.println("Done!");
+
         }
     }
 }
