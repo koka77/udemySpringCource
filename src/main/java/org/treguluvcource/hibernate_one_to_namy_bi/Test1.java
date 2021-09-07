4db34e36c1fc69385f2a3d657cbab753e51c0d29
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2021-09-07 15:11:16 +0300

    fetch  type EAGER and LAZY если лейзи выборка, то после закрытия сессии (коммита) не удасться получить данные из связанной таблицы. для избежания этого можно до закрытия сессии получить эти данные, см. пример

--- src/main/java/org/treguluvcource/hibernate_one_to_namy_bi/Test1.java
+++ src/main/java/org/treguluvcource/hibernate_one_to_namy_bi/Test1.java
@@ -16,13 +16,15 @@ public class Test1 {
                 addAnnotatedClass(Department.class).
                 buildSessionFactory();
              Session session = factory.getCurrentSession()) {
-//
-//            Department dep = new Department("IT", 1200, 300);
+
+//            Department dep = new Department("Sales", 1500, 800);
 //            Employee emp1 = new Employee("Alex", "Somov", 800);
-//            Employee emp2 = new Employee("Helen", "Smirnova", 1000);
+//            Employee emp2 = new Employee("Helen", "Smirnova", 1500);
+//            Employee emp3 = new Employee("Antony", "Sidorov", 1200);
 //
 //            dep.addEmployeeToDepartment(emp1);
 //            dep.addEmployeeToDepartment(emp2);
+//            dep.addEmployeeToDepartment(emp3);
 //
 //            session.beginTransaction();
 //
@@ -31,13 +33,31 @@ public class Test1 {
 //            session.getTransaction().commit();
 //            System.out.println("Done!");
 
+//*********************************************
 
             session.beginTransaction();
 
 //            получение департамента с работниками
-//            Department department = session.get(Department.class, 1);
-//            System.out.println(department);
-//            System.out.println(department.getEmps());
+            System.out.println("Get department");
+            Department department = session.get(Department.class, 3);
+
+
+            System.out.println("Show department");
+            System.out.println(department);
+
+//            для избежания нульпоинтера можем перед закрытием спессии подгрузить работников
+            System.out.println("Подгружаем наших работников");
+            department.getEmps().get(0);
+
+
+            session.getTransaction().commit();
+
+            System.out.println("Show empoloyees of the department");
+            System.out.println(department.getEmps());
+            System.out.println("Done!");
+
+
+//*********************************************
 
 //            получение департамента по работнику
 //            Employee employee = session.get(Employee.class, 1);
@@ -45,11 +65,11 @@ public class Test1 {
 //            System.out.println(employee.getDepartment());
 
 //            ВНИМАНИЕ! удалит все записи из за каскадов в обоих таблицах
-            Employee employee = session.get(Employee.class, 1);
-            session.delete(employee);
-
-            session.getTransaction().commit();
-            System.out.println("Done!");
+//            Employee employee = session.get(Employee.class, 1);
+//            session.delete(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
         }
     }
 }
