5aa2b76f58a1cbb08043d5c8929ff50c367b438b
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2021-09-07 18:12:37 +0300

    Many_to_many part 4 Сохранение данных в бд при помощи метода persist работает без каскада типа ALL

--- src/main/java/org/treguluvcource/hibernate_many_to_many/Test1.java
+++ src/main/java/org/treguluvcource/hibernate_many_to_many/Test1.java
@@ -17,22 +17,18 @@ public class Test1 {
                 Session session = factory.getCurrentSession()) {
 
 
-            Section section1 = new Section("Football");
-            Child child1 = new Child("Alex", 5);
-            Child child2 = new Child("Masha", 7);
-            Child child3 = new Child("Vasya", 6);
-
-            session.beginTransaction();
-            session.save(section1);
+            Section section1 = new Section("Dance");
+            Child child1 = new Child("Olya", 12);
+            Child child2 = new Child("Grisha", 8);
+            Child child3 = new Child("Pavlik", 9);
 
             section1.addChildToSection(child1);
             section1.addChildToSection(child2);
             section1.addChildToSection(child3);
 
-            session.save(child1);
-            session.save(child2);
-            session.save(child3);
+            session.beginTransaction();
 
+            session.persist(section1);
 
             session.getTransaction().commit();
             System.out.println("Done!");
