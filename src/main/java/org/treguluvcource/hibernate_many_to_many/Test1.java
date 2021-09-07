6ea69803b702265557137e6c6f7d15be4047bf15
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2021-09-07 18:15:47 +0300

    many_to_many part 5 удаление из бд записи без удаления связной записи при этом связи из таблицы связей тоже удалятся

--- src/main/java/org/treguluvcource/hibernate_many_to_many/Test1.java
+++ src/main/java/org/treguluvcource/hibernate_many_to_many/Test1.java
@@ -16,22 +16,22 @@ public class Test1 {
                         buildSessionFactory();
                 Session session = factory.getCurrentSession()) {
 
-
-            Section section1 = new Section("Dance");
-            Child child1 = new Child("Olya", 12);
-            Child child2 = new Child("Grisha", 8);
-            Child child3 = new Child("Pavlik", 9);
-
-            section1.addChildToSection(child1);
-            section1.addChildToSection(child2);
-            section1.addChildToSection(child3);
-
-            session.beginTransaction();
-
-            session.persist(section1);
-
-            session.getTransaction().commit();
-            System.out.println("Done!");
+//
+//            Section section1 = new Section("Dance");
+//            Child child1 = new Child("Olya", 12);
+//            Child child2 = new Child("Grisha", 8);
+//            Child child3 = new Child("Pavlik", 9);
+//
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child2);
+//            section1.addChildToSection(child3);
+//
+//            session.beginTransaction();
+//
+//            session.persist(section1);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
 //********************************************************
 
 //            Section section1 = new Section("Volleyball");
@@ -72,6 +72,15 @@ public class Test1 {
 //
 //            session.getTransaction().commit();
 //            System.out.println("Done!");
+//********************************************************
+            //
+            session.beginTransaction();
+            Section section = session.get(Section.class, 8);
+
+            session.delete(section);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
 //********************************************************
         }
     }
